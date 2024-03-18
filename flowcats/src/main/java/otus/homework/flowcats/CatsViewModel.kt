b46b59685e1eeafb516addb20619a5053691453c
s.krivosheev <s.krivosheev@qiwi.com>
2024-03-18 22:26:20 +0300

    homework 2
    (cherry picked from commit 5aa8465f84a2d44399e31826cc025e76c18ae9ca)

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,31 +1,56 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _factUiState = MutableStateFlow<CatFactsUiState>(
+        CatFactsUiState.Success(
+            Fact(
+                createdAt = "",
+                deleted = false,
+                id = "",
+                text = "",
+                source = "",
+                used = false,
+                type = "",
+                user = "",
+                updatedAt = ""
+            )
+        )
+    )
+    val factUiState: StateFlow<CatFactsUiState> = _factUiState
 
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+            catsRepository.listenForCatFacts()
+                .flowOn(Dispatchers.IO)
+                .catch {
+                    _factUiState.value = CatFactsUiState.Error(it)
+                }.collect {
+                    _factUiState.value = CatFactsUiState.Success(it)
                 }
-            }
         }
     }
 }
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
+}
+
+sealed class CatFactsUiState {
+    data class Success(val fact: Fact) : CatFactsUiState()
+    data class Error(val exception: Throwable) : CatFactsUiState()
 }
